707ed71ae5357c4ab7514859a7cd8bc00a7f8ebf
MekkiZ <jmekki@gmail.com>
2021-09-16 23:20:23 +0200

    commit sur main

---
+++ Tkinterbeginpy.py
@@ -0,0 +1,27 @@
+#coding:utf-8
+
+
+import tkinter
+
+
+"""
+commentaire important sur le sujet :
+
+oon peut cre une fenetre principal avec la methode constructuer
+
+un widget est un fenetre, une interface qu'on cree exempl ace tkinter  : mainapp = tkinter.Tk()
+                                                                        mainapp.mainloop()          
+
+
+
+
+
+
+
+"""
+
+mainapp = tkinter.Tk()
+mainapp.mainloop()
+
+
+
